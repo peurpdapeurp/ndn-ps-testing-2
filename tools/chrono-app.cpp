a67ca968f034f3259a22ed1f345a8d47deba0f46
Edward Lu <edwardzlu98@gmail.com>
2018-07-20 18:36:46 +0000

    tse

--- tools/chrono-app.cpp
+++ tools/chrono-app.cpp
@@ -147,10 +147,10 @@ protected:
   void
   insertIntoRepo(const Name& dataName) {
     try {
-      std::string dataType = dataName.get(-3).toUri().substr(1);
+      std::string dataType = dataName.get(-3).toUri();
       if (dataType == "temperature" || dataType == "light" || dataType == "humidity" || dataType == "pressure" || dataType == "resistance") {}
       else {
-	std::cout << "Data name's third from last component was: " << dataName.get(-3).toUri().substr(1) << ", returning..." << std::endl;
+	std::cout << "Data name's third from last component was: " << dataName.get(-3).toUri() << ", returning..." << std::endl;
         return;
       }
     } catch (const std::exception& e) {
